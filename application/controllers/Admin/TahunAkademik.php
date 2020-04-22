63d26b3bfe6deb45e70cc6e1d90d5ab1b8ea8357
JepriAlber <jeprialber@gmail.com>
2020-04-22 15:34:40 +0700

    membuat form tambah tahun akademik

--- application/controllers/Admin/TahunAkademik.php
+++ application/controllers/Admin/TahunAkademik.php
@@ -7,13 +7,23 @@ class TahunAkademik extends CI_Controller
 	
 	public function index()
 	{
-		$datajudul['judul'] 		= 'Jurusan';
+		$datajudul['judul'] 		= 'Tahun Akademik';
 		$data['tahunakademik']	 	= $this->TahunAkademik_model->Tampil_data('tahun_akademik')->result();
 
 			$this->load->view('includesAdmin/header',$datajudul);
  			$this->load->view('includesAdmin/navbar');
  			$this->load->view('includesAdmin/sidebar');
  			$this->load->view('admin/tahun_akademik',$data);
+ 			$this->load->view('includesAdmin/footer');
+	}
+
+	public function input()
+	{
+		$datajudul['judul']		= 'Tambah Tahun Akademik';
+		$this->load->view('includesAdmin/header',$datajudul);
+ 			$this->load->view('includesAdmin/navbar');
+ 			$this->load->view('includesAdmin/sidebar');
+ 			$this->load->view('admin/form_tahun_akademik');
  			$this->load->view('includesAdmin/footer');
 	}
 
---
+++ application/views/admin/form_tahun_akademik.php
@@ -0,0 +1,52 @@
+<!-- Main Content -->
+<div class="main-content">
+  <section class="section">
+    <div class="section-header">
+      <i class="fas fa-university mr-2"></i><h1> Tahun Akademik</h1>
+    </div>
+
+    <div class="section-body">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-12">
+          <div class="card">
+            <div class="card-header">
+              <h4>Tambah Tahun Akademik</h4>
+            </div>
+            <div class="card-body">
+                <form action="<?=base_url('admin/tahun_akademik/input_aksi')?>" method="POST">
+
+                <div class="form-group">
+                  <label for="tahun_akademik">Tahun Akademik :</label>
+                  <input type="text" name="tahun_akademik" class="form-control " id="tahun_akademik">
+                  <small class="form-text text-danger"><?=form_error('tahun_akademik'); ?></small>
+                </div>
+
+                <div class="form-group">
+                  <label for="semester">Semester :</label>
+                  <select name="semester" id="semester" class="form-control">
+                    <option>-Pilih-</option>
+                    <option value="Ganjil">Ganjil</option>
+                    <option value="Genap">Genap</option>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="status">Status :</label>
+                  <select name="status" id="status" class="form-control">
+                    <option>-Pilih-</option>
+                    <option value="Aktif">Aktif</option>
+                    <option value="Tidak Aktif">Tidak Aktif</option>
+                  </select>
+                </div>
+
+                <div class="card-footer text-right">
+                    <button class="btn btn-primary mr-1" type="submit" onclick="return confirm('Apakah ada yakin ingin Simpan data ini?')">Simpan</button>
+                  </div>     
+                </form>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
