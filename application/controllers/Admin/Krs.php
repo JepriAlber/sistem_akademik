8ef4d53df747367e538594127f25cb865eccb89e
JepriAlber <jeprialber@gmail.com>
2020-04-26 17:07:24 +0700

    Menyelesaikan fitur KRS

--- application/controllers/Admin/Krs.php
+++ application/controllers/Admin/Krs.php
@@ -111,5 +111,115 @@ class Krs extends CI_Controller
  			$this->load->view('includesAdmin/footer');
 	}
 
+	public function tambah_krs_aksi()
+	{
+		$this->_rules();
+
+			if ($this->form_validation->run() == FALSE) {
+				$this->tambah_krs($this->input->post('npm',TRUE), $this->input->post('id_ta'));
+			} else {
+				$npm 				= $this->input->post('npm',TRUE);
+				$id_ta				= $this->input->post('id_ta',TRUE);
+				$kode_matakuliah	= $this->input->post('kode_matakuliah', TRUE);
+
+					$data 	= array(
+						'npm'				=> $npm,
+						'id_ta'			=> $id_ta,
+						'kode_matakuliah'	=> $kode_matakuliah
+					);
+
+				$this->Krs_model->Insert_data($data);
+				$this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show" role="alert">
+		              <strong>Data KHS Berhasil Ditambah!</strong>
+		              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		                <span aria-hidden="true">&times;</span>
+		              </button>
+		            </div>');
+ 				redirect('admin/krs/index');
+			}	
+	}
+
+	public function _rules()
+	{
+		$this->form_validation->set_rules('npm','NPM','required');
+		
+		$this->form_validation->set_rules('id_ta','id_ta','required');
+
+		$this->form_validation->set_rules('kode_matakuliah','Kode Matakuliah','required',[
+			'required' => 'Kode Matakuliah Tidak Boleh Kosong!'
+		]);
+	}
+
+	public function update($id)
+	{
+		$row 	= $this->Krs_model->get_by_id($id);
+		$th 	= $row->id_ta;
+
+			if ($row) {
+					
+					$data 	= array(
+							'id_krs'  			=> set_value('id_krs', $row->id_krs),
+							'id_ta'  			=> set_value('id_ta', $row->id_ta),
+							'npm'				=> set_value('npm', $row->npm),
+							'kode_matakuliah'	=> set_value('kode_matakuliah', $row->kode_matakuliah),
+							'tahun_akademik'	=> $this->TahunAkademik_model->get_by_id($th)->tahun_akademik,
+							'semester'			=> $this->TahunAkademik_model->get_by_id($th)->semester==1?'Ganjil':'Genap'
+						);
+
+					$datajudul['judul']		= 'Tambah Data KRS';
+
+						$this->load->view('includesAdmin/header',$datajudul);
+			 			$this->load->view('includesAdmin/navbar');
+			 			$this->load->view('includesAdmin/sidebar');
+			 			$this->load->view('admin/edit_krs',$data);
+			 			$this->load->view('includesAdmin/footer');
+
+			} else {
+				$this->session->set_flashdata('pesan','<div class="alert alert-warning alert-dismissible fade show" role="alert">
+		              <strong>Data Tidak Ditemukan!</strong>
+		              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		                <span aria-hidden="true">&times;</span>
+		              </button>
+		            </div>');
+ 				redirect('admin/krs/index');
+			}	
+	}
+
+	public function update_krs_aksi()
+	{
+		$id_krs				= $this->input->post('id_krs',TRUE);
+		$npm 				= $this->input->post('npm',TRUE);
+		$id_ta 				= $this->input->post('id_ta',TRUE);
+		$kode_matakuliah	= $this->input->post('kode_matakuliah',TRUE);
+
+			$data   = array(
+				'id_krs'			=> $id_krs,
+				'id_ta'				=> $id_ta,
+				'npm'				=> $npm,
+				'kode_matakuliah'	=> $kode_matakuliah
+			);
+		$this->Krs_model->Update_data($id_krs,$data);
+		$this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show" role="alert">
+		              <strong>Data KRS Berhasil Diedit!</strong>
+		              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		                <span aria-hidden="true">&times;</span>
+		              </button>
+		            </div>');
+ 				redirect('admin/krs/index');
+	}
+
+	public function hapus($id)
+	{
+		$where 		=array('id_krs' => $id);
+		$this->Krs_model->Hapus_data($where);
+		$this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show" role="alert">
+		              <strong>Data KRS Berhasil DiHapus!</strong>
+		              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		                <span aria-hidden="true">&times;</span>
+		              </button>
+		            </div>');
+ 				redirect('admin/krs/index');
+	}
+
 }
 ?>
--- application/models/Krs_model.php
+++ application/models/Krs_model.php
@@ -4,6 +4,30 @@
  */
 class Krs_model extends CI_Model
 {
-	
+
+	public $table   = 'krs';
+	public $id 		= 'id_krs';
+
+	public function Insert_data($data)
+	{
+		$this->db->insert($this->table,$data);
+	}
+
+	public function get_by_id($id)
+	{
+		$this->db->where($this->id,$id);
+		return $this->db->get($this->table)->row();
+	}
+
+	public function Update_data($id, $data)
+	{
+		$this->db->where($this->id,$id);
+		$this->db->update($this->table,$data);
+	}
+	public function Hapus_data($where)
+	{
+		$this->db->where($where);
+		$this->db->delete($this->table);
+	}
 }
 ?>
